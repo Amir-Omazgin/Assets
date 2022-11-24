85f5a191d1f6325fa09ce66985a3e7f7c6661777
Amir Omazgin <amir.o@quali.com>
2022-11-24 10:22:11 +0200

    added directory

---
+++ terraform/storageAzure/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.32.0"
+    }
+  }
+}
+
+provider "azurerm" {
+ features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "my-example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "justaname"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = "content"
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "blob" {
+  name                   = "my-awesome-content.zip"
+  storage_account_name   = azurerm_storage_account.storage.name
+  storage_container_name = azurerm_storage_container.container.name
+  type                   = "Block"
+  # source                 = "some-local-file.zip"
+}
