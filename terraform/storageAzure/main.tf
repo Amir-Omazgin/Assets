49348d44ae9fb210d24c868a9f741837eaceb50b
amir.o <amir.o@quali.com>
2022-12-14 16:23:04 +0200

    added type

--- terraform/storageAzure/main.tf
+++ terraform/storageAzure/main.tf
@@ -34,6 +34,6 @@ resource "azurerm_storage_blob" "blob" {
   name                   = "my-awesome-content.zip"
   storage_account_name   = azurerm_storage_account.storage.name
   storage_container_name = azurerm_storage_container.container.name
-
+  type                   = "block"
   # source                 = "some-local-file.zip"
 }
